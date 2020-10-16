8acee3afe50d75034d2abb9c057ceed846357142
eggertanja97 <egger.tanja97@gmail.com>
2020-10-16 15:21:29 +0200

    V1.1

--- MyHelloWorldApp/MyHelloWorldApp/Program.cs
+++ MyHelloWorldApp/MyHelloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace MyHelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2");
         }
     }
 }
